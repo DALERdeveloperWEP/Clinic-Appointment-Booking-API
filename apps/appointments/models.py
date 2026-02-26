222458cfdf2ba22cfe8aa9a8e444f80c155d6bbf
DALERdeveloperWEP <cassidymlbbbbb@gmail.com>
2026-02-26 15:20:37 +0500

    Appointment models

--- apps/appointments/models.py
+++ apps/appointments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
-# Create your models here.
+from ..doctors.models import TimeSlot
+from ..users.models import User
+class Appointment(models.Model):
+    class StatusChoice(models.TextChoices):
+        Pending = 'pending', 'Pending' 
+        Confirmed = 'confirmed', 'Confirmed' 
+        Cancelled = 'cancelled', 'Cancelled'
+        
+    doctor = models.ForeignKey(User, related_name='appointment', on_delete=models.CASCADE)
+    patient = models.ForeignKey(User, related_name='appointment', on_delete=models.CASCADE)
+    timeslot = models.OneToOneField(TimeSlot)
+    status = models.CharField(choices=StatusChoice, default=StatusChoice.Pending)
+    created_at = models.DateTimeField(auto_now_add=True)
